28c1e0ed140582a945c7901884ee137e271dc138
niqing <18827391015@sin.com.cn>
2019-09-27 19:47:34 +0800

    nq 9.27

--- mpvueapp/src/pages/index/index.vue
+++ mpvueapp/src/pages/index/index.vue
@@ -1,126 +1,179 @@
 <template>
-  <div @click="clickHandle">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
-
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
+  <div class="main">
+    <div class="top-map"></div>
+    <div class="beijing">
+      <div class="userinfo">
+        <div class="touxiang">
+          <img src="../img/photo.png" alt />
         </div>
-        <div class="right">
+        <div class="xinxi">
+          <p>倪庆</p>
+          <p style="font-size:14px;color:#333;">
+            <img src="../img/biao.png" alt /> 武汉HTML1906期
+          </p>
         </div>
+        <div class="tj">
+          <p>统计</p>
+        </div>
+      </div>
+      <div class="content">
+        <div class="quan">
+          <div class="one" style="font-weight: 600">签到</div>
+          <div class="two" style="font-size:13px">14:40:12</div>
+        </div>
+        <div class="fanwei">已进入考勤范围</div>
+        <div class="bottom-img">
+          <img src="../img/jianying.jpg" alt />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
+// import card from '@/components/card'
 
 export default {
-  data () {
+  data() {
     return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
-    }
+      txt: "打卡首页"
+      // motto: 'Hello miniprograme',
+      // userInfo: {
+      //   nickName: 'mpvue',
+      //   avatarUrl: 'http://mpvue.com/assets/logo.png'
+      // }
+    };
   },
 
   components: {
-    card
+    // card
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
-    }
+    // bindViewTap () {
+    //   const url = '../logs/main'
+    //   if (mpvuePlatform === 'wx') {
+    //     mpvue.switchTab({ url })
+    //   } else {
+    //     mpvue.navigateTo({ url })
+    //   }
+    // },
+    // clickHandle (ev) {
+    //   console.log('clickHandle:', ev)
+    //   // throw {message: 'custom test'}
+    // }
   },
 
-  created () {
+  created() {
     // let app = getApp()
   }
-}
+};
 </script>
 
 <style scoped>
-.userinfo {
+.main {
+  width: 100%;
+  height: 100vh;
+  /* background: red; */
   display: flex;
   flex-direction: column;
-  align-items: center;
+  position: relative;
 }
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
+.top-map {
+  width: 100%;
+  height: 40%;
+  background-color: yellow;
+}
+.beijing {
+  width: 100%;
+  flex: 1;
+  background: #d0e4fc;
+  /* display: flex;
+  justify-content: center; */
+}
+.userinfo {
+  width: 98%;
+  /* height: 150rpx; */
+  height: 12%;
+  background: #fff;
+  border-radius: 15px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  /* top: 0; */
+  /* bottom: 620rpx; */
+  bottom: 56%;
+  margin: auto;
+}
+.content {
+  width: 98%;
+  /* height: 600rpx; */
+  height: 54%;
+  background-color: #fff;
+  border-radius: 15px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  /* top: 0; */
+  bottom: 10rpx;
+  margin: auto;
+}
+.touxiang {
+  width: 15%;
+  height: 70%;
+  border: 2rpx solid #b6befa;
   border-radius: 50%;
+  float: left;
+  margin-top: 2%;
+  margin-left: 30rpx;
 }
-
-.userinfo-nickname {
-  color: #aaa;
+.touxiang img {
+  width: 100%;
+  height: 100%;
 }
-
-.usermotto {
-  margin-top: 150px;
+.xinxi {
+  float: left;
+  margin-left: 30rpx;
+  color: black;
+  margin-top: 2%;
 }
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
+.xinxi img {
+  width: 16px;
+  height: 16px;
 }
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
+.tj {
+  float: right;
+  font-size: 20px;
+  color: #4b8ff7;
+  margin-right: 40rpx;
+  margin-top: 5%;
 }
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
+.quan {
+  width: 200rpx;
+  height: 200rpx;
+  border-radius: 50%;
+  background: #498ef7;
+  margin: 0 auto;
+  margin-top: 40rpx;
+  text-align: center;
+  color: #fff;
+  line-height: 80rpx;
 }
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
+.fanwei {
+  width: 100%;
+  text-align: center;
+  margin-top: 30rpx;
+  font-weight: 600;
 }
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
+.bottom-img {
+  width: 100%;
+  height: 190rpx;
+  
+  position: absolute;
+  bottom: 10px;
+}
+.bottom-img img{
+  width: 100%;
+  height: 100%;
 }
 </style>
