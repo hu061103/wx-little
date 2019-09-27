6819ebc8d91c6eeeb6e9f378905d28348e6239f1
hubicheng <2074360374@qq.com>
2019-09-27 11:44:26 +0800

    xmkj

---
+++ mpvueapp/src/pages/mine/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>mine===mine</h2>
+        <h2>{{msg}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg:"vue"
+        }
+    }
+}
+</script>
---
+++ mpvueapp/src/pages/shenqing/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>sq===sq</h2>
+        <h2>{{msg}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg:"shengqing"
+        }
+    }
+}
+</script>
